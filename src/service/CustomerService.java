2bc2b3ff01667edcf33701cfc3266c1c13060291
arlw87 <arlw87@googlemail.com>
2022-01-31 20:53:21 +0000

    Add comments to the classes in the service package

--- src/service/CustomerService.java
+++ src/service/CustomerService.java
@@ -6,6 +6,10 @@ import java.util.Collection;
 import java.util.ArrayList;
 import java.util.DuplicateFormatFlagsException;
 
+/**
+ * Stateful Class that provides the storage of Customer Data using the Singleton Pattern
+ */
+
 public class CustomerService {
 
     private static CustomerService singleton = null;
@@ -16,6 +20,10 @@ public class CustomerService {
         System.out.println("The singleton has been created");
     }
 
+    /**
+     * Creates and / or returns the CustomerService Singleton
+     * @return
+     */
     public static CustomerService getInstance(){
         if (singleton == null){
             singleton = new CustomerService();
@@ -23,6 +31,13 @@ public class CustomerService {
         return singleton;
     }
 
+    /**
+     * Adds a customer to the Hotel Reservation system
+     * @param email
+     * @param firstName
+     * @param lastName
+     * @throws IllegalArgumentException if the email is not valid
+     */
     public void addCustomer(String email, String firstName, String lastName) throws IllegalArgumentException{
         if (getCustomer(email) != null){
             throw new IllegalArgumentException("Customer email already in use");
@@ -47,6 +62,10 @@ public class CustomerService {
         return null;
     }
 
+    /**
+     * Returns all customers
+     * @return ALl Customers
+     */
     public Collection<Customer> getAllCustomers(){
         return customers;
     }
--- src/service/ReservationService.java
+++ src/service/ReservationService.java
@@ -4,6 +4,11 @@ import java.util.*;
 
 import model.*;
 
+/**
+ * Stateful class that provides the storage for Reservations in the hotel reservation system using the
+ * singlton pattern.
+ */
+
 public class ReservationService {
 
     private static ReservationService singleton = null;
@@ -12,9 +17,12 @@ public class ReservationService {
     private static Collection<Reservation> reservations = new ArrayList<>();
 
     private ReservationService(){
-        System.out.println("A Singleton has been created for the Reservation service");
     };
 
+    /**
+     * Returns and / or creates a singleton object
+     * @return
+     */
     public static ReservationService getInstance(){
         if (singleton == null){
             singleton = new ReservationService();
@@ -22,32 +30,24 @@ public class ReservationService {
         return singleton;
     }
 
+    /**
+     * Add room to the Hotel Reservation System
+     * @param room
+     */
     public void addRoom(IRoom room){
-        //using put will overwrite the value if the key already exist
-        //this behaviour seems sensible as you may want to upper your room info
         rooms.put(room.getRoomNumber(), room);
     }
 
     /**
-     * For Testing purposes to add multiple rooms quickly
-     * @param rooms
-     */
-    public void addRooms(IRoom ...rooms){
-        for (IRoom r: rooms)
-        {
-            addRoom(r);
-        }
-    }
-
-    /**
-     * For Testing purposes
+     * Get all rooms
+     * @return All rooms as a collection
      */
     public Collection<IRoom> getAllRooms(){
         return rooms.values();
     }
 
     /***
-     *
+     * Room data stored as HashMap so use roomID to return room object
      * @param roomID
      * @return The room, or null if no room with that roomID is found
      */
@@ -55,10 +55,12 @@ public class ReservationService {
         return rooms.get(roomID);
     }
 
+    /**
+     * Iterate through all the customers reserves and return the ones that match the customer provided
+     * @param customer
+     * @return Collection of customer Objects, empty is no matches for supplied customer
+     */
     public Collection<Reservation> getCustomersReservation(Customer customer){
-        //iterate through all of the reservations and return a collection of
-        //reservation made by that customer
-
         Collection<Reservation> customersReservations = new ArrayList<>();
 
         for (Reservation r: reservations){
@@ -70,48 +72,65 @@ public class ReservationService {
         return customersReservations;
     }
 
+    /**
+     * Create a reservation object and add it to the List of reservations
+     * @param customer
+     * @param room
+     * @param chekInDate
+     * @param checkOutDate
+     * @return The reservation that has just been created
+     */
     public Reservation reserveARoom(Customer customer, IRoom room, Date chekInDate, Date checkOutDate){
-        //check for if the room is free on those dates should be done before
         Reservation r = new Reservation(customer, room, chekInDate, checkOutDate);
-        //Add to the reservation list
         reservations.add(r);
         return r;
     }
 
+    /**
+     * Find available rooms based on the check in and check out dates supplied. Room is available
+     * if it exists and doesn't have a reservation that overlaps the supplied check in and check out dates
+     * @param checkInDate
+     * @param checkOutDate
+     * @return A collection of rooms. If no room found a collection of rooms with size 0
+     */
     public Collection<IRoom> findRooms(Date checkInDate, Date checkOutDate){
-        //need to find all rooms that havent been reserved at all
-        //need to find all rooms that havent been reserved for the dates specified
-
         //create a list of all rooms
         Collection<IRoom> allRooms = rooms.values();
+        //empty list to store all available rooms
         List<IRoom> availableRooms = new ArrayList<IRoom>();
 
         OUTER_LOOP: for (IRoom room : allRooms){
-            //now loop through the current reservations and check against rooms list
+            //loop through the current reservations and check against rooms list
             INNER_LOOP: for (Reservation r: reservations){
-                //room in reservation same as the current room in room list
+                //if room in reservation same as the current room in room list does the reserveration dates overlap
+                //the checkInDate and checkOutDate provided
                 if (r.getRoom().getRoomNumber().equals(room.getRoomNumber())){
-                    //Do the rate ranges overlap
                     if (doDateRangesOverlap(checkInDate, checkOutDate, r.getCheckIn(), r.getCheckOut())){
                         //if the reservation overlaps with requested dates
-                        //then dont add room to available rooms and goto next room in list
+                        //then dont add room to available rooms as it is not available
+                        //goto next room in all rooms list
                         continue OUTER_LOOP;
                     }
                 }
             }
-            //Add room to available rooms
+            //Add room to available room if it doesnt have any reservationa ginst it or its reservations
+            //dont fall within the chekInDate and checkOutDate
             availableRooms.add(room);
         }
 
         return availableRooms;
     }
 
+    /**
+     * Helper function to work out if two date ranges overlap or not
+     * @param rangeAStart
+     * @param rangeAEnd
+     * @param rangeBStart
+     * @param rangeBEnd
+     * @return True if they do overlap, false if they do not
+     */
     private boolean doDateRangesOverlap(Date rangeAStart, Date rangeAEnd, Date rangeBStart, Date rangeBEnd){
         return (rangeAStart.getTime() <= rangeBEnd.getTime() && rangeAEnd.getTime() >= rangeBStart.getTime());
-    }
-
-    public Collection<Reservation> debugReturnReservations(){
-        return reservations;
     }
 
     public void printAllReservations(){
@@ -125,7 +144,5 @@ public class ReservationService {
             System.out.println(r);
         }
     }
-
-
 
 }
