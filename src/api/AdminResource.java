9747ac3adb9af54b1620bf18dc8120a7407ba2f1
arlw87 <arlw87@googlemail.com>
2022-01-31 22:07:49 +0000

    Add comments to the Admin Resource Class in API package

--- src/api/AdminResource.java
+++ src/api/AdminResource.java
@@ -5,7 +5,8 @@ import java.util.*;
 
 /**
  * Provides an API for the admin functions of the Hotel Reservation system, between the UI (Admin menu) and the
- * Service classes
+ * Service classes. AdminResource uses the singleton pattern so that only one instance of it, is accessible to the
+ * program
  */
 public class AdminResource {
 
@@ -18,6 +19,10 @@ public class AdminResource {
         System.out.println("Created Singleton for AdminResource");
     }
 
+    /**
+     * Creates and / or returns the single instance of the Admin Resource
+     * @return Singleton of the AdminResource class
+     */
     public static AdminResource getInstance(){
         if (singleton == null){
             singleton = new AdminResource();
@@ -27,10 +32,19 @@ public class AdminResource {
         return singleton;
     }
 
+    /**
+     * Get Customer Object using the customers unique email
+     * @param email of the customer
+     * @return The Customer object associated with the customers unique email
+     */
     public Customer getCustomer(String email){
         return cs.getCustomer(email);
     }
 
+    /**
+     * Add one or more rooms to the the Hotel Reservation System
+     * @param rooms
+     */
     public void addRooms(List<IRoom> rooms){
         for (IRoom r: rooms)
         {
@@ -38,13 +52,25 @@ public class AdminResource {
         }
     }
 
+    /**
+     * Return all rooms in the Hotel Reservation System
+     * @return All room objects in a Collection
+     */
     public Collection<IRoom> getAllRooms(){
         return rs.getAllRooms();
     }
 
+    /**
+     * Return all Customer objects saved in the Hotel Reservation System
+     * @return
+     */
     public Collection<Customer> getAllCustomers(){
         return cs.getAllCustomers();
     }
+
+    /**
+     * Display all previously created reservations
+     */
     public void displayAllReservations(){
         rs.printAllReservations();
     }
