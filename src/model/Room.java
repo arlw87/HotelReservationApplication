3ae8bd30ef076bceb360e3e305293cde784bda75
arlw87 <arlw87@googlemail.com>
2022-02-01 10:52:16 +0000

    Update Room model class to have final data types

--- src/model/Room.java
+++ src/model/Room.java
@@ -4,9 +4,9 @@ import java.util.Objects;
 
 public class Room implements IRoom{
 
-    private String roomNumber;
-    private Double price;
-    private RoomType roomType;
+    private final String roomNumber;
+    private final Double price;
+    private final RoomType roomType;
 
     public Room(String roomNumber, Double price, RoomType roomType){
         this.roomNumber = roomNumber;
